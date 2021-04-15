46776cbeae74c12e4ad1e8ddbe88086248557d9a
Vishakha Roy <69618079+VishRoy@users.noreply.github.com>
2021-04-16 00:41:08 +0530

    Time: 76 ms (88.81%), Space: 39.1 MB (30.27%) - LeetHub

--- running-sum-of-1d-array/running-sum-of-1d-array.js
+++ running-sum-of-1d-array/running-sum-of-1d-array.js
@@ -3,8 +3,8 @@
  * @return {number[]}
  */
 var runningSum = function(nums) {
+    let a = 0;
     let sum = 0
-    let a = 0
     for(let i=0;i<nums.length;i++){
         sum += nums[i]
         nums[a] = sum
