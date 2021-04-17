f4aa69cb620223f4a2c8eaba0af3ea108988e7ab
Vishakha Roy <69618079+VishRoy@users.noreply.github.com>
2021-04-17 17:23:00 +0530

    Time: 88 ms (43.47%), Space: 41 MB (98.52%) - LeetHub

---
+++ valid-mountain-array/valid-mountain-array.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} arr
+ * @return {boolean}
+ */
+var validMountainArray = function(arr) {
+    if(arr.length < 3){ return false;}
+    let i = 0;
+    
+    while(i < arr.length && i+1 < arr.length && arr[i] < arr[i+1]){
+       i++;
+    }
+    
+    if(i== 0 || i+1 >= arr.length){
+        return false;
+    }
+    while(i < arr.length && i+1 < arr.length){
+        if(arr[i] <= arr[i++ + 1]){
+            return false;
+        }
+    }
+    
+    return true
+};
