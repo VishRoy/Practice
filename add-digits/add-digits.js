3ec2aeb8bbfd6782b3295266d7c07ebafb644745
Vishakha Roy <69618079+VishRoy@users.noreply.github.com>
2021-04-25 23:48:44 +0530

    Time: 108 ms (18.82%), Space: 40.5 MB (23.87%) - LeetHub

---
+++ add-digits/add-digits.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number} num
+ * @return {number}
+ */
+var addDigits = function(num) {
+     while(num.toString().length >= 2) {
+        let str = num.toString()
+        let currentSum = 0
+        
+        for(const digit of str) {
+            currentSum += Number(digit)    
+        }
+        
+        num = currentSum
+    }
+    
+    return num
+};
