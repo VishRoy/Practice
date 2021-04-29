098ef9342c3a9b4db91831458cb3a066e18b0ff6
Vishakha Roy <69618079+VishRoy@users.noreply.github.com>
2021-04-29 21:31:41 +0530

    Update to-lower-case.js

--- to-lower-case/to-lower-case.js
+++ to-lower-case/to-lower-case.js
@@ -3,5 +3,16 @@
  * @return {string}
  */
 var toLowerCase = function(str) {
-    return str.replace(/[A-Z]/g,function(b){return String.fromCharCode(b.charCodeAt()+32)})
+//     return str.replace(/[A-Z]/g,function(b){return String.fromCharCode(b.charCodeAt()+32)})
+    
+    let answer = "";
+  for (const i of str) {
+    if (65 <= i.charCodeAt() && i.charCodeAt() <= 90) {
+      answer += String.fromCharCode(i.charCodeAt() + 32);
+    } else {
+      answer += i;
+    }
+  }
+
+  return answer;
 };
