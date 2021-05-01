7b3de07d4f024f48dfd43923d6630268f09efd1b
Vishakha Roy <69618079+VishRoy@users.noreply.github.com>
2021-05-01 20:44:35 +0530

    Time: 120 ms (6.61%), Space: 39.2 MB (47.04%) - LeetHub

---
+++ jewels-and-stones/jewels-and-stones.js
@@ -0,0 +1,10 @@
+/**
+ * @param {string} jewels
+ * @param {string} stones
+ * @return {number}
+ */
+var numJewelsInStones = function(jewels, stones) {
+     const REGEX = new RegExp(`[${jewels}]`,'g')
+    
+     return stones.match(REGEX) ? stones.match(REGEX).length : 0;
+};
