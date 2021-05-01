4dde1c65a8f243a826995161fae8fe04c9e7a212
Vishakha Roy <69618079+VishRoy@users.noreply.github.com>
2021-05-01 20:47:39 +0530

    Update

--- jewels-and-stones/jewels-and-stones.js
+++ jewels-and-stones/jewels-and-stones.js
@@ -4,7 +4,18 @@
  * @return {number}
  */
 var numJewelsInStones = function(jewels, stones) {
-     const REGEX = new RegExp(`[${jewels}]`,'g')
+      const jewelsSet = new Set()
+    let count = 0
+    for (let i = 0; i < jewels.length; i++) {
+        jewelsSet.add(jewels[i])
+    }
     
-     return stones.match(REGEX) ? stones.match(REGEX).length : 0;
+    for (let i = 0; i < stones.length; i++) {
+        if (jewelsSet.has(stones[i])) count++
+    }
+    
+    return count
+//      const REGEX = new RegExp(`[${jewels}]`,'g')
+    
+//      return stones.match(REGEX) ? stones.match(REGEX).length : 0;
 };
